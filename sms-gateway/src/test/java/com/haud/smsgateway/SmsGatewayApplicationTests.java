82cfc0efc5f34a32cff1d4818f58f6742441cb6e
Mhretaab <mhretaab.berhe@uniken.com>
2022-02-04 12:17:11 +0300

    first commit

---
+++ sms-gateway/src/test/java/com/haud/smsgateway/SmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haud.smsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spam-filter/src/main/java/com/haud/spamfilter/SpamFilterApplication.java
@@ -0,0 +1,13 @@
+package com.haud.spamfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamFilterApplication.class, args);
+	}
+
+}
